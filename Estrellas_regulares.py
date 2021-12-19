a4dc0abce50707be50c1d300b98922695872d907
Ana López <34817139+AnaLopezP@users.noreply.github.com>
2021-12-19 19:59:11 +0100

    funcion

--- Estrellas_regulares.py
+++ Estrellas_regulares.py
@@ -1,6 +1,17 @@
 import turtle
 from turtle import *
-
+def dibujar_estrella(puntas, color_1, color_2):
+    color(color_1, color_2)
+    begin_fill()
+    angulo = 180 - (180/int(puntas))
+    print(angulo)
+    while True:
+        forward(200)
+        left(angulo)
+        if abs(pos()) < 1:
+            break
+    end_fill()
+    done()
 
 print("¿De cuantas puntas quieres tu estrella? Minimo de 5 puntas.")
 puntas = input()
