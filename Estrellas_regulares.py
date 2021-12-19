f9aadbe157f84a50ec4d97b8765e464a689f5869
Ana López <34817139+AnaLopezP@users.noreply.github.com>
2021-12-19 19:43:11 +0100

    planteamiento codigo principal

---
+++ Estrellas_regulares.py
@@ -0,0 +1,10 @@
+import turtle
+from turtle import *
+
+
+print("¿De cuantas puntas quieres tu estrella? Minimo de 5 puntas.")
+puntas = input()
+print("Y de que dos colores la quieres?")
+color_1 = input()
+color_2 = input()
+dibujar_estrella(puntas, color_1, color_2)
