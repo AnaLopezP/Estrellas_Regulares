4d84f491ef4e274c4e48e53fedaae59033f44fc6
Ana López <34817139+AnaLopezP@users.noreply.github.com>
2021-12-19 21:53:56 +0100

    actualizaciones y pruebas en la funcion

--- Estrellas_regulares.py
+++ Estrellas_regulares.py
@@ -3,8 +3,12 @@ from turtle import *
 def dibujar_estrella(puntas, color_1, color_2):
     color(color_1, color_2)
     begin_fill()
-    angulo = 180 - (180/int(puntas))
-    print(angulo)
+    puntas_int = int(puntas)
+    cociente, resto = divmod(puntas_int, 2)
+    if resto == 0:
+        puntas_int = puntas_int/2
+    
+    angulo = 180 - (180/puntas_int)
     while True:
         forward(200)
         left(angulo)
@@ -13,7 +17,7 @@ def dibujar_estrella(puntas, color_1, color_2):
     end_fill()
     done()
 
-print("¿De cuantas puntas quieres tu estrella? Minimo de 5 puntas.")
+print("¿De cuantas puntas quieres tu estrella? Minimo de 5 puntas e impar. Si pones un numero par saldrá el doble, ya que las puntas pares no se pueden dibujar de una sola trazada.")
 puntas = input()
 print("Y de que dos colores la quieres?")
 color_1 = input()
